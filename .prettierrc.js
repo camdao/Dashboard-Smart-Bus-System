00a8262e5e2f25b04921c6e69caa7f09995a0ece
camdao <75600481+camdao@users.noreply.github.com>
2025-10-10 18:46:50 +0700

    config: storybook (#10)
    * add storybook
    
    * fix: prettier error

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-export default {
-    singleQuote: true,
-    semi: true,
-    tabWidth: 2,
-    useTabs: false,
-    trailingComma: "all",
-    printWidth: 120,
-    arrowParens: "always",
-};
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,31 @@
-// eslint.config.mjs
-import js from "@eslint/js";
-import nextPlugin from "@next/eslint-plugin-next";
-import tseslint from "typescript-eslint";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import reactPlugin from "eslint-plugin-react";
-import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
-import prettier from "eslint-config-prettier";
+import js from '@eslint/js';
+import nextPlugin from '@next/eslint-plugin-next';
+import prettier from 'eslint-config-prettier';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import reactPlugin from 'eslint-plugin-react';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import storybook from 'eslint-plugin-storybook';
+import tseslint from 'typescript-eslint';
 
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
   prettier,
   {
-    ignores: [".next/", "node_modules/", "public", ".yarn", "@types"],
+    ignores: ['.next/', 'node_modules/', 'public', '.yarn', '@types'],
 
     plugins: {
-      "@next/next": nextPlugin,
+      '@next/next': nextPlugin,
       react: reactPlugin,
-      "jsx-a11y": jsxA11yPlugin,
-      "simple-import-sort": simpleImportSort,
+      'jsx-a11y': jsxA11yPlugin,
+      'simple-import-sort': simpleImportSort,
     },
 
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
         ecmaVersion: 2022,
-        sourceType: "module",
+        sourceType: 'module',
         ecmaFeatures: {
           jsx: true,
         },
@@ -33,69 +33,60 @@ export default [
     },
 
     rules: {
-      // ✅ React & Next rules
-      "react/react-in-jsx-scope": "off",
-      "react/no-unknown-property": ["error", { ignore: ["css"] }],
-      "react/button-has-type": "error",
-      "react/jsx-no-useless-fragment": ["error", { allowExpressions: true }],
-      "react/jsx-filename-extension": [
-        "warn",
-        { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-      ],
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      ...storybook.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/no-unknown-property': ['error', { ignore: ['css'] }],
+      'react/button-has-type': 'error',
+      'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+      'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
 
-      // ✅ Accessibility rules
-      "jsx-a11y/label-has-associated-control": [
-        "error",
+      'jsx-a11y/label-has-associated-control': [
+        'error',
         {
-          labelComponents: ["label"],
-          labelAttributes: ["label"],
-          controlComponents: ["StyledHiddenInput"],
+          labelComponents: ['label'],
+          labelAttributes: ['label'],
+          controlComponents: ['StyledHiddenInput'],
           depth: 1,
         },
       ],
 
-      // ✅ Import sorting rules
-      "simple-import-sort/imports": [
-        "error",
+      'simple-import-sort/imports': [
+        'error',
         {
           groups: [
-            ["^react$", "^next", "^@", "^[a-z]"],
-            ["^~"],
-            [
-              "^\\.\\.(?!/?$)",
-              "^\\.\\./?$",
-              "^\\./(?=.*/)(?!/?$)",
-              "^\\.(?!/?$)",
-              "^\\./?$",
-            ],
-            ["^\\u0000"],
+            ['^react$', '^next', '^@', '^[a-z]'],
+            ['^~'],
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$', '^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            ['^\\u0000'],
           ],
         },
       ],
-      "simple-import-sort/exports": "error",
+      'simple-import-sort/exports': 'error',
 
-      // ✅ TypeScript rules
-      "@typescript-eslint/consistent-type-imports": [
-        "error",
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
         {
-          prefer: "type-imports",
-          fixStyle: "inline-type-imports",
+          prefer: 'type-imports',
+          fixStyle: 'inline-type-imports',
         },
       ],
-      "@typescript-eslint/no-shadow": "error",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
+      '@typescript-eslint/no-shadow': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
         {
           ignoreRestSiblings: true,
-          argsIgnorePattern: "_",
-          varsIgnorePattern: "_",
+          argsIgnorePattern: '_',
+          varsIgnorePattern: '_',
         },
       ],
-      "@typescript-eslint/naming-convention": [
-        "error",
-        { selector: "interface", format: ["PascalCase"] },
-        { selector: "typeAlias", format: ["PascalCase"] },
+      '@typescript-eslint/naming-convention': [
+        'error',
+        { selector: 'interface', format: ['PascalCase'] },
+        { selector: 'typeAlias', format: ['PascalCase'] },
       ],
     },
   },
+  ...storybook.configs['flat/recommended'],
+  ...storybook.configs['flat/recommended'],
 ];
