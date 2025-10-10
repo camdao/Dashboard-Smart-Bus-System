fd1bb646dd705d26637d9c21ebeecc8d04055e70
camdao <luongcamdao@gmail.com>
2025-10-10 21:58:57 +0700

    feat: add color

---
+++ src/styles/color.ts
@@ -0,0 +1,52 @@
+import { defineTokens } from '@pandacss/dev';
+
+export const colorList = defineTokens({
+  colors: {
+    blue: {
+      100: { value: '#5570F1' },
+      90: { value: '#6078EC' },
+      80: { value: '#6D83EC' },
+      70: { value: '#7C8FEC' },
+      60: { value: '#8899E9' },
+      50: { value: '#97A5EB' },
+      40: { value: '#ABB5E9' },
+      30: { value: '#B6BFE8' },
+      20: { value: '#C4CAE8' },
+      10: { value: '#DBDEEE' },
+    },
+
+    peach: {
+      100: { value: '#FFCC91' },
+      90: { value: '#FFD29E' },
+      80: { value: '#FFDAAE' },
+      70: { value: '#FFDFBA' },
+      60: { value: '#FFE5C8' },
+      50: { value: '#FFEAD1' },
+      40: { value: '#FFF0DE' },
+      30: { value: '#FFF2E2' },
+      20: { value: '#FEF5EA' },
+      10: { value: '#FEF9F2' },
+    },
+
+    gray: {
+      100: { value: '#1C1D22' },
+      90: { value: '#2C2D33' },
+      80: { value: '#33343A' },
+      70: { value: '#37393F' },
+      60: { value: '#45464E' },
+      50: { value: '#53545C' },
+      40: { value: '#6E7079' },
+      30: { value: '#8B8D97' },
+      20: { value: '#A6A8B1' },
+      10: { value: '#BEC0CA' },
+    },
+
+    green: {
+      100: { value: '#519C66' },
+    },
+
+    red: {
+      100: { value: '#CC5F5F' },
+    },
+  },
+});
