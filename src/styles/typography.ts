80bf042354a74b19e0bac806207e4d1f051dd77d
camdao <75600481+camdao@users.noreply.github.com>
2025-10-10 23:42:12 +0700

    feat: add font (#12)

---
+++ src/styles/typography.ts
@@ -0,0 +1,103 @@
+import { defineTextStyles } from '@pandacss/dev';
+
+export const typographyList = defineTextStyles({
+  h1Bold: {
+    value: { fontFamily: 'poppins', fontWeight: 'bold', fontSize: '64px', lineHeight: '1.2' },
+  },
+  h1Medium: {
+    value: { fontFamily: 'poppins', fontWeight: 'medium', fontSize: '64px', lineHeight: '1.2' },
+  },
+  h1Regular: {
+    value: { fontFamily: 'poppins', fontWeight: 'normal', fontSize: '64px', lineHeight: '1.2' },
+  },
+  h2Bold: {
+    value: { fontFamily: 'poppins', fontWeight: 'bold', fontSize: '52px', lineHeight: '1.2' },
+  },
+  h2Medium: {
+    value: { fontFamily: 'poppins', fontWeight: 'medium', fontSize: '52px', lineHeight: '1.2' },
+  },
+  h2Regular: {
+    value: { fontFamily: 'poppins', fontWeight: 'normal', fontSize: '52px', lineHeight: '1.2' },
+  },
+  h3Bold: {
+    value: { fontFamily: 'poppins', fontWeight: 'bold', fontSize: '44px', lineHeight: '1.2' },
+  },
+  h3Medium: {
+    value: { fontFamily: 'poppins', fontWeight: 'medium', fontSize: '44px', lineHeight: '1.2' },
+  },
+  h3Regular: {
+    value: { fontFamily: 'poppins', fontWeight: 'normal', fontSize: '44px', lineHeight: '1.2' },
+  },
+  sh1Bold: {
+    value: { fontFamily: 'poppins', fontWeight: 'bold', fontSize: '32px', lineHeight: '1.2' },
+  },
+  sh1Medium: {
+    value: { fontFamily: 'poppins', fontWeight: 'medium', fontSize: '32px', lineHeight: '1.2' },
+  },
+  sh1Regular: {
+    value: { fontFamily: 'poppins', fontWeight: 'normal', fontSize: '32px', lineHeight: '1.2' },
+  },
+  sh2Bold: {
+    value: { fontFamily: 'poppins', fontWeight: 'bold', fontSize: '28px', lineHeight: '1.2' },
+  },
+  sh2Medium: {
+    value: { fontFamily: 'poppins', fontWeight: 'medium', fontSize: '28px', lineHeight: '1.2' },
+  },
+  sh2Regular: {
+    value: { fontFamily: 'poppins', fontWeight: 'normal', fontSize: '28px', lineHeight: '1.2' },
+  },
+  sh3Bold: {
+    value: { fontFamily: 'poppins', fontWeight: 'bold', fontSize: '20px', lineHeight: '1.2' },
+  },
+  sh3Medium: {
+    value: { fontFamily: 'poppins', fontWeight: 'medium', fontSize: '20px', lineHeight: '1.2' },
+  },
+  sh3Regular: {
+    value: { fontFamily: 'poppins', fontWeight: 'normal', fontSize: '20px', lineHeight: '1.2' },
+  },
+  p1Bold: {
+    value: { fontFamily: 'poppins', fontWeight: 'bold', fontSize: '16px', lineHeight: '1.2' },
+  },
+  p1Medium: {
+    value: { fontFamily: 'poppins', fontWeight: 'medium', fontSize: '16px', lineHeight: '1.2' },
+  },
+  p1Regular: {
+    value: { fontFamily: 'poppins', fontWeight: 'normal', fontSize: '16px', lineHeight: '1.2' },
+  },
+  p2Bold: {
+    value: { fontFamily: 'poppins', fontWeight: 'bold', fontSize: '14px', lineHeight: '1.2' },
+  },
+  p2Medium: {
+    value: { fontFamily: 'poppins', fontWeight: 'medium', fontSize: '14px', lineHeight: '1.2' },
+  },
+  p2Regular: {
+    value: { fontFamily: 'poppins', fontWeight: 'normal', fontSize: '14px', lineHeight: '1.2' },
+  },
+  label1Bold: {
+    value: { fontFamily: 'poppins', fontWeight: 'bold', fontSize: '12px', lineHeight: '1.2' },
+  },
+  label1Medium: {
+    value: { fontFamily: 'poppins', fontWeight: 'medium', fontSize: '12px', lineHeight: '1.2' },
+  },
+  label1Regular: {
+    value: { fontFamily: 'poppins', fontWeight: 'normal', fontSize: '12px', lineHeight: '1.2' },
+  },
+  label2Bold: {
+    value: { fontFamily: 'poppins', fontWeight: 'bold', fontSize: '11px', lineHeight: '1.2' },
+  },
+  label2Medium: {
+    value: { fontFamily: 'poppins', fontWeight: 'medium', fontSize: '11px', lineHeight: '1.2' },
+  },
+  label2Regular: {
+    value: { fontFamily: 'poppins', fontWeight: 'normal', fontSize: '11px', lineHeight: '1.2' },
+  },
+  label3Bold: {
+    value: { fontFamily: 'poppins', fontWeight: 'bold', fontSize: '10px', lineHeight: '1.2' },
+  },
+  label3Medium: {
+    value: { fontFamily: 'poppins', fontWeight: 'medium', fontSize: '10px', lineHeight: '1.2' },
+  },
+  label3Regular: {
+    value: { fontFamily: 'poppins', fontWeight: 'normal', fontSize: '10px', lineHeight: '1.2' },
+  },
+});
